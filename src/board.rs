bd7aaf47c2c3bdc7018871b3f5abd313756e5bdc
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-15 23:52:04 +0300

    Implementing make_move

--- src/board.rs
+++ src/board.rs
@@ -7,18 +7,25 @@ pub fn base_pos(p: Player) -> (usize, usize) {
     }
 }
 
-fn place_base(p: Player, t: &mut Tiles) {
-    let (x, y) = base_pos(p);
-    t[x][y] = Tile::Base(p);
-}
-
 pub fn create_board() -> Tiles {
     let mut res = [[Tile::Empty; BOARD_HEIGHT]; BOARD_WIDTH];
+
+    fn place_base(p: Player, t: &mut Tiles) {
+        let (x, y) = base_pos(p);
+        t[x][y] = Tile::Base(p);
+    }
 
     place_base(Player::Red, &mut res);
     place_base(Player::Blue, &mut res);
 
     res
+}
+
+pub fn make_move(board: &mut Tiles, player: Player, x: usize, y: usize) {
+    // TODO: Somehow encode valid moves into a type,
+    // and make_move takes that type as arg, so valid move is guaranteed?
+    let tile = board[x][y];
+
 }
 
 #[cfg(test)]
