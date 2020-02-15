2651a2453c0b1c9970435bfa1dc0d6fb6603269b
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-15 23:44:41 +0300

    Print board

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,37 @@ mod board;
 mod models;
 
 use crate::board::*;
+use crate::models::*;
 
 fn main() {
     let board = create_board();
-    println!("Board: {:#?}", board);
+    let board_str = print_board(board);
+    println!("{}", board_str);
+}
+
+fn print_board(board: Tiles) -> String {
+    fn get_ch(tile: Tile) -> char {
+        match tile {
+            Tile::Empty => '.',
+            Tile::Base(Player::Blue) => '⬛',
+            Tile::Base(Player::Red) => '⬤',
+            Tile::Alive(Player::Blue) => '◻',
+            Tile::Alive(Player::Red) => '○',
+            Tile::Squashed(Player::Blue) => '◼',
+            Tile::Squashed(Player::Red) => '●',
+        }
+    }
+
+    let mut res = String::new();
+
+    for y in 0..BOARD_HEIGHT {
+        for x in 0..BOARD_WIDTH {
+            let tile = board[x][y];
+            let ch = get_ch(tile);
+            res.push(ch);
+        }
+        res.push('\n');
+    }
+
+    res
 }
