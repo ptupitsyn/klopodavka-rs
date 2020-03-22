01b463224d78eb9316ed2853979ff9da8ce298ad
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-03-22 20:26:00 +0300

    Weight-base advance_move implemented

--- src/ai.rs
+++ src/ai.rs
@@ -26,55 +26,40 @@ fn attack_move(game: &GameState) -> Option<TilePos> {
 }
 
 fn advance_move(game: &GameState) -> Option<TilePos> {
-    let has_one_neighbor = |p: Pos| {
-        board::neighbors(p)
-            .iter()
-            .filter(|&&n| !game.tile(n).is_empty())
-            .count()
-            == 1
-    };
-
-    moves(game).find(|&t| t.tile.is_empty() && has_one_neighbor(t.pos))
+    moves(game).min_by(|&x, &y| weight(game, x.pos).cmp(&weight(game, y.pos)))
 }
 
 fn random_move(game: &GameState) -> Option<TilePos> {
     moves(game).choose(&mut rand::thread_rng())
 }
 
-fn get_advance_moves_with_weights(game: &GameState) -> Vec<(Pos, u16)> {
+fn weight(game: &GameState, pos: Pos) -> u16 {
     // Compute move weights based on:
     // * Neighbor count - less is better
     // * Diagonal - true is better
     // * Enemy base distance - less is better
 
-    let mut res: Vec<(Pos, u16)> = Vec::new();
+    let neighbs = board::neighbors(pos);
+
+    let nonempty_neighbs = neighbs
+        .iter()
+        .filter(|n| !game.tile(**n).is_empty())
+        .count();
+
+    let diag_neighbs = neighbs
+        .iter()
+        .filter(|n| n.x != pos.x && n.y != pos.y && !game.tile(**n).is_empty())
+        .count();
+
+    let nondiag_neighbs = nonempty_neighbs - diag_neighbs;
+
+    // TODO: Base positions should be saved within GameState
     let enemy_base = board::base_pos(game.current_player().other());
+    let base_dist = dist(enemy_base, pos);
 
-    for mv in moves(game) {
-        let pos = mv.pos;
+    let weight = diag_neighbs + nondiag_neighbs * 2 + base_dist as usize;
 
-        let neighbs = board::neighbors(pos);
-
-        let nonempty_neighbs = neighbs
-            .iter()
-            .filter(|n| !game.tile(**n).is_empty())
-            .count();
-
-        let diag_neighbs = neighbs
-            .iter()
-            .filter(|n| n.x != pos.x && n.y != pos.y && !game.tile(**n).is_empty())
-            .count();
-
-        let nondiag_neighbs = nonempty_neighbs - diag_neighbs;
-
-        let base_dist = dist(enemy_base, pos);
-
-        let weight = diag_neighbs + nondiag_neighbs * 2 + base_dist as usize;
-
-        res.push((pos, weight as u16))
-    }
-
-    res
+    weight as u16
 }
 
 fn dist(a: Pos, b: Pos) -> u16 {
