1ba3aca70c7131753ee997cdfcadd1337ae39a83
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-03-24 15:20:50 +0300

    Adding automatic deployment with Travis to GitHub pages

--- crates/klopodavka-yew/build.sh
+++ crates/klopodavka-yew/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-wasm-pack build --target web && rollup ./main.js --format iife --file ./pkg/bundle.js
+wasm-pack build --target web
+
+cp ./main.js pkg
+
+cp pkg/*.wasm www
+cp pkg/*.js www
+cp ./main.js www
+cd www || exit
+rollup main.js --format iife --file bundle.js
--- crates/klopodavka-yew/main.js
+++
@@ -1,6 +0,0 @@
-import init, { run_app } from './pkg/klopodavka_yew.js';
-async function main() {
-   await init('./pkg/klopodavka_yew_bg.wasm');
-   run_app();
-}
-main();
